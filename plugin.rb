c6633e5d29db813c0d2f28d674ce05d050f3d968
KAJATIGER <ksantro@gmx.de>
2017-07-28 13:47:20 +0200

    still had some uncomitted changes here from yesterday

--- plugin.rb
+++ plugin.rb
@@ -25,7 +25,6 @@ require 'sidekiq'
 enabled_site_setting :discourse_backups_enabled
 
 after_initialize do
-	load File.expand_path("../lib/synchronizer.rb", __FILE__)
   load File.expand_path("../app/jobs/regular/sync_backups_to_drive.rb", __FILE__)
   load File.expand_path("../lib/drive_synchronizer.rb", __FILE__)
 
